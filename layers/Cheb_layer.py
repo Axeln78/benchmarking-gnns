127118660727f44125be274116a8f078f596860a
Axel Bernard Gunnar Nilsson <axel.nilsson@epfl.ch>
2020-05-05 16:25:30 +0200

    Linting and simplification pep8

--- layers/Cheb_layer.py
+++ layers/Cheb_layer.py
@@ -9,53 +9,53 @@ from dgl.nn.pytorch import GraphConv
 """
     Cheb
 """
-    
-# Sends a message of node feature h
-# Equivalent to => return {'m': edges.src['h']}
-msg = fn.copy_src(src='h', out='m')
-
 
 class NodeApplyModule(nn.Module):
     """Update the node feature hv with ReLU(Whv+b)."""
 
-    def __init__(self, in_feats, out_feats, k, activation, bias=True):
+    def __init__(self, in_feats, out_feats, k):
         super(NodeApplyModule, self).__init__()
-        self.linear = nn.Linear(k * in_feats, out_feats, bias)
-        self.activation = activation
+        self.linear = nn.Linear(k * in_feats, out_feats)
 
     def forward(self, node):
         h = self.linear(node.data['h'])
-        if self.activation:
-            h = self.activation(h)
         return {'h': h}
-    
-    
+
 
 class ChebLayer(nn.Module):
     """
         Param: [in_dim, out_dim]
     """
-    def __init__(self, in_dim, out_dim, k, activation, dropout, graph_norm, batch_norm, residual):
+
+    def __init__(
+            self,
+            in_dim,
+            out_dim,
+            k,
+            activation,
+            dropout,
+            graph_norm,
+            batch_norm,
+            residual=False):
         super().__init__()
         self.in_channels = in_dim
         self.out_channels = out_dim
         self.graph_norm = graph_norm
         self.batch_norm = batch_norm
+        self.residual = residual
         self._k = k
-        
+
         if in_dim != out_dim:
             self.residual = False
-        
+
         self.batchnorm_h = nn.BatchNorm1d(out_dim)
         self.activation = activation
         self.dropout = nn.Dropout(dropout)
         self.apply_mod = NodeApplyModule(
             in_dim,
             out_dim,
-            k=self._k,
-            activation=activation)
+            k=self._k)
 
-        
     def forward(self, g, feature, snorm_n, lambda_max=None):
         h_in = feature   # to be used for residual connection
 
@@ -74,7 +74,7 @@ class ChebLayer(nn.Module):
                     lambda_max = dgl.laplacian_lambda_max(g)
                 except BaseException:
                     # if the largest eigonvalue is not found
-                    lambda_max = torch.Tensor(2).to(feature.device)
+                    lambda_max = [2]
 
             if isinstance(lambda_max, list):
                 lambda_max = torch.Tensor(lambda_max).to(feature.device)
@@ -91,16 +91,16 @@ class ChebLayer(nn.Module):
             if self._k > 1:
                 re_norm = (2. / lambda_max).to(feature.device)
                 h = unnLaplacian(X_0, D_sqrt, g)
-                #print('h',h,'norm',re_norm,'X0',X_0)
+                # print('h',h,'norm',re_norm,'X0',X_0)
                 X_1 = - re_norm * h + X_0 * (re_norm - 1)
-                
+
                 Xt = torch.cat((Xt, X_1), 1)
 
             # Xi(x), i = 2...k
-            for i in range(2, self._k):
+            for _ in range(2, self._k):
                 h = unnLaplacian(X_1, D_sqrt, g)
                 X_i = - 2 * re_norm * h + X_1 * 2 * (re_norm - 1) - X_0
-                
+
                 Xt = torch.cat((Xt, X_i), 1)
                 X_1, X_0 = X_i, X_1
 
@@ -110,22 +110,20 @@ class ChebLayer(nn.Module):
             h = g.ndata.pop('h')
 
         if self.graph_norm:
-            h = h * snorm_n # normalize activation w.r.t. graph size
+            h = h * snorm_n  # normalize activation w.r.t. graph size
 
         if self.batch_norm:
-            h = self.batchnorm_h(h) # batch normalization  
-       
+            h = self.batchnorm_h(h)  # batch normalization
+
         if self.activation:
             h = self.activation(h)
-            
+
         if self.residual:
-            h = h_in + h # residual connection
-        
-            
+            h = h_in + h  # residual connection
+
         h = self.dropout(h)
         return h
-    
+
     def __repr__(self):
-        return '{}(in_channels={}, out_channels={}, k={})'.format(self.__class__.__name__,
-                                             self.in_channels,
-                                             self.out_channels,self._k)
+        return '{}(in_channels={}, out_channels={}, k={})'.format(
+            self.__class__.__name__, self.in_channels, self.out_channels, self._k)
